b236faace848c5bf8e9cd3c2529014e8eb559c5a
Susmita428 <166370882+Susmita428@users.noreply.github.com>
2025-04-20 20:58:38 +0530

    Add app/src/main/java/com/example/ecommerceapp/Product.java 1745162917298

---
+++ app/src/main/java/com/example/ecommerceapp/Product.java
@@ -0,0 +1,37 @@
+package com.example.ecommerceapp;
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private int imageResource;
+
+    public Product(String id, String name, String description, double price, int imageResource) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageResource = imageResource;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+}
